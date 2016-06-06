ac1430ba58f6cec4f29a3acc50b7499a613a0f4c
Gaston Corrao <gaston.corrao@mercadolibre.com>
2016-06-06 18:12:04 -0300

    changed dockerfiles

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for image in $(ls); do
-  [[ -d $image ]] && echo "building ${image}..." && cd $image && docker build -t $image . && cd ..
+  [[ -d $image ]] && echo -e "\n-----------------\nbuilding ${image}..." && cd $image && docker build -t $image . && cd ..
 done
